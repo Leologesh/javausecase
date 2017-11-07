cd78c6c7a2506ef971be3ce6c82ac6da8031aea8
AnanthaSivaMani <ananthasivamani@gmail.com>
2017-11-07 17:22:55 +0530

    079

--- SpencerHome/src/in/vamsoft/spencer/Customer.java
+++ SpencerHome/src/in/vamsoft/spencer/Customer.java
@@ -1,5 +1,42 @@
 package in.vamsoft.spencer;
 
 public class Customer {
+	int custId;
+	String name;
+	int contactNo;
+	public Customer() {
+		// TODO Auto-generated constructor stub
+	}
+	public Customer(int custId, String name, int contactNo) {
+		super();
+		this.custId = custId;
+		this.name = name;
+		this.contactNo = contactNo;
+	}
+	public int getCustId() {
+		return custId;
+	}
+	public void setCustId(int custId) {
+		this.custId = custId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getContactNo() {
+		return contactNo;
+	}
+	public void setContactNo(int contactNo) {
+		this.contactNo = contactNo;
+	}
+	
+	public void addCustomer() {
+		
+	}
+	public void bookProduct(Product obj) {
+		
+	}
 
 }
--- SpencerHome/src/in/vamsoft/spencer/RetailStore.java
+++ SpencerHome/src/in/vamsoft/spencer/RetailStore.java
@@ -1,5 +1,33 @@
 package in.vamsoft.spencer;
 
-public class RetailStore {
+public class RetailStore extends Customer {
 
+	String storeName;
+	public static int custId=1;
+	public static int pId=1;
+	public RetailStore(String name) {
+		super();
+	}
+	public void addCustomer(String name,int contact) {
+		Customer c =new Customer(custId,name,contact);
+		custId++;
+	}
+	public void addProduct(String pName,String status,double price,long quantity) {
+		Product p = new Product(pId,pName,status,quantity);
+	}
+	public int checkProductAvailability(String productName) {
+		return 1;
+		
+	}
+	public int bookProduct(int customerID,String productName,int qtyOfProduct) {
+		return 1;
+		
+	}
+	public double calculateNetAmount(int customerID,double discount) {
+		return discount;
+		
+	}
+	
+	
+		
 }
